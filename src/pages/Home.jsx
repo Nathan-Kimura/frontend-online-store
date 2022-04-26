d001a624f5326d3e0a43ae81b02fe28800fd978b
Thalys RD <tgbr66@gmail.com>
2022-04-26 17:32:38 -0300

    Requisito 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Categories from '../components/Categories';
 
 class Home extends Component {
   render() {
     return (
       <section>
         <Header />
+        <Categories />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
